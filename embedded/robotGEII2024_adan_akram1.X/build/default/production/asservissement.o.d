ea63f92e08e239348aa7df6368892918516620ca
iutgeiitoulonE105 <70626387+iutgeiitoulonE105@users.noreply.github.com>
2025-09-03 08:35:45 +0200

    01/09/09

--- embedded/robotGEII2024_adan_akram1.X/build/default/production/asservissement.o.d
+++ embedded/robotGEII2024_adan_akram1.X/build/default/production/asservissement.o.d
@@ -1,1 +1,19 @@
-build/default/production/asservissement.o: asservissement.c
+build/default/production/asservissement.o: asservissement.c \
+ asservissement.h QEI.h timer.h IO.h UART_Protocol.h UART.h Utilities.h \
+ robot.h
+
+asservissement.h:
+
+QEI.h:
+
+timer.h:
+
+IO.h:
+
+UART_Protocol.h:
+
+UART.h:
+
+Utilities.h:
+
+robot.h:
